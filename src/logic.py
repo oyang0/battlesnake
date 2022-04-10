954a7f304f06427b283d48b83acfe760132ba8ac
Oliver Yang <oyang@ualberta.ca>
2022-04-10 05:35:29 -0600

    Moved NNUE

--- src/logic.py
+++ src/logic.py
@@ -1,7 +1,6 @@
 import random
 from typing import List, Dict
 
-import numpy as np
 from pickle import load
 from copy import deepcopy
 
@@ -11,51 +10,6 @@ This file can be a nice home for your Battlesnake's logic and helper functions.
 We have started this for you, and included some logic to remove your Battlesnake's 'neck'
 from the list of possible moves!
 """
-
-
-class NNUE:
-    "An efficiently updatable neural network for evaluation"
-
-    def __init__(self, ft_weight, ft_bias, l1_weight, l1_bias, l2_weight, l2_bias):
-        self.ft_weight = ft_weight
-        self.ft_bias = ft_bias
-        self.l1_weight = l1_weight
-        self.l1_bias = l1_bias
-        self.l2_weight = l2_weight
-        self.l2_bias = l2_bias
-
-        self.accumulator = None
-        self.refresh_accumulator([])
-
-    def foward(self, features=None):
-        accumulator = self._ft(features) if features else self.accumulator
-        l1_x = self._relu(accumulator)
-        l2_x = self._relu(self._l1(l1_x))
-
-        return self._l2(l2_x)
-
-    def refresh_accumulator(self, active_features):
-        self.accumulator = self.ft_bias.copy()
-        for active_feature in active_features:
-            self.accumulator += self.ft_weight[:, active_feature]
-
-    def update_accumulator(self, removed_features, added_features):
-        for removed_feature in removed_features:
-            self.accumulator -= self.ft_weight[:, removed_feature]
-        for added_feature in added_features:
-            self.accumulator += self.ft_weight[:, added_feature]
-
-    def _ft(self, features):
-        return self.ft_weight @ features + self.ft_bias
-
-    def _l1(self, features):
-        return self.l1_weight @ features + self.l1_bias
-
-    def _l2(self, features):
-        return self.l2_weight @ features + self.l2_bias
-
-    def _relu(self, features):
-        return np.maximum(0, features)
 
 
 def _get_feature_mapping() -> dict:
@@ -130,7 +84,7 @@ moves = {0: "left", 1: "right", 2: "down", 3: "up"}
 games = {}
 models = []
 
-fo = open("nnue.pth", "rb")
+fo = open("src/nnue.pth", "rb")
 nnue = load(fo)
 fo.close()
 del fo
